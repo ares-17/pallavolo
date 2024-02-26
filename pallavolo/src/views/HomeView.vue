45f9ea1347127670a7a40890aa50fb0788dd8531
Alessandro <schiavo.alessandro@protonmail.com>
2024-02-26 15:39:32 +0100

    add vue 3 skeleton

---
+++ pallavolo/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    Hello!
+</template>
+
+<script lang="ts">
+export default {
+    name: 'HomeView',
+    setup() {
+        return {
+
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style></style>
---
+++ pallavolo/src/views/OpponentDetail.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'OpponentDetail',
+    setup() {
+        return {
+
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style></style>
